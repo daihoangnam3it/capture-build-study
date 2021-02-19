ec39adf05adc3de37e3eed80c6c41fb72546d580
Hoàng Đại <daihoangnam3it@gmail.com>
2021-02-19 15:32:29 +0700

    About

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import home1 from '../img/home1.png'
+
+import styled from 'styled-components'
+
 const AboutSection = () => {
   return (
-    <div>
-      <div className='description'>
+    <About>
+      <Description>
         <div className='title'>
-          <div className='hide'>
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className='hide'>
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className='hide'>
+          </Hide>
+          <Hide>
             <h2>true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
@@ -24,10 +27,39 @@ const AboutSection = () => {
           sapiente.
         </p>
         <button>Contact us</button>
-      </div>
-      <img src={home1} alt="cameraman"/>
-    </div>
+      </Description>
+      <Image>
+        <img src={home1} alt="cameraman"/>
+      </Image>
+    </About>
   );
 };
+
+const About=styled.div`
+  min-height:90vh;
+  display:flex;
+  align-items:center;
+  justify-content:space-between;
+  padding:5rem 10rem;
+  color:white;
+`
+const Description=styled.div`
+  flex:1;
+  padding-right:5rem;
+  h2{
+    font-weight:lighter;
+  }
+`
+const Image=styled.div`
+  flex:1;
+  img{
+    width:100%;
+    height:80vh;
+    object-fit:cover;
+  }
+`
+const Hide=styled.div`
+  overflow:hidden;
+`
 
 export default AboutSection;
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,60 @@
+import {createGlobalStyle} from 'styled-components'
+const GlobalStyle=createGlobalStyle`
+:root{
+  --color-body:#1b1b1b;
+  --border-color-button:#23d997;
+  --color-button:#fff;
+  --color-p:#ccc;
+  --color-dreams:#23d997;
+}
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box
+  }
+  html{
+    font-size:62.5%;
+  }
+  body{
+    background:var(--color-body);
+  }
+  button{
+    font-weight:bold;
+    font-size:1.1rem;
+    cursor: pointer;
+    padding:1rem 2rem;
+    border:3px solid var(--border-color-button);
+    background-color:transparent;
+    color:var(--color-button);
+    transition:all .25s linear;
+    &:hover{
+      background-color:var(--border-color-button);
+    }
+  }
+  h2{
+    font-weight:lighter;
+    font-size:5rem;
+  }
+  h3{
+    color:var(--color-button);
+  }
+  h4{
+    font-weight:bold;
+  }
+  a{
+    font-size:1.1rem;
+  }
+  span{
+    color:var(--color-dreams);
+    font-weight:bold;
+  }
+  p{
+    padding:3rem 0;
+    color:var(--color-p);
+    font-size:1.4rem;
+    line-height:150%;
+  }
+
+`
+
+export default GlobalStyle;
